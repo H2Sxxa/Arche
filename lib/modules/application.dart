3044d1aee6441b6146b89c3f3ef6fc4fd9007ff7
H2Sxxa <h2sxxa0w0@gmail.com>
2023-12-30 14:00:52 +0800

    🐛 export type

--- lib/modules/application.dart
+++ lib/modules/application.dart
@@ -1,4 +1,5 @@
 export 'package:arche/src/application/bus.dart' show ArcheBus;
 export 'package:arche/src/application/config.dart' show ArcheConfig;
 export 'package:arche/src/application/logger.dart' show ArcheLogger;
-export 'package:arche/src/application/translator.dart' show Translator;
+export 'package:arche/src/application/translator.dart'
+    show Translator, EnumStringTranslator, StringTranslator;
